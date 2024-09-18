c65d66898a6dc1b0c106849da2e68163554e7f66
Kevin McCormack <kevin@mccormack.tech>
2024-09-18 00:04:15 -0400

    Add first test

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,22 @@
 use std::{thread, time};
 
 use chrono;
-use colored::Colorize;
+use colored::{ColoredString, Colorize};
 use clap::Parser;
+use reqwest::StatusCode;
 
 #[derive(Parser)]
 struct Cli {
     url_string: String,
+}
+
+fn colorized_status(status: StatusCode) -> ColoredString {
+    return match status.as_u16() {
+        200..=299 => status.to_string().green(),
+        400..=499 => status.to_string().yellow(),
+        500..=599 => status.to_string().red(),
+        _ => status.to_string().normal(),
+    };
 }
 
 #[tokio::main]
@@ -26,15 +36,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .status();
 
         let now = chrono::offset::Local::now();
-        let colored_status = match status.as_u16() {
-            200..=299 => status.to_string().green(),
-            400..=499 => status.to_string().yellow(),
-            500..=599 => status.to_string().red(),
-            _ => status.to_string().normal(),
-        };
+        let colored_status = colorized_status(status);
         println!("{} Response status: {}", now, colored_status);
         thread::sleep(five_seconds);
     }
 
     // Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_colorized_status() {
+        assert_eq!(colorized_status(StatusCode::OK), "200 OK".green());
+        assert_eq!(colorized_status(StatusCode::NOT_FOUND), "404 Not Found".yellow());
+        assert_eq!(colorized_status(StatusCode::INTERNAL_SERVER_ERROR), "500 Internal Server Error".red());
+    }
 }
