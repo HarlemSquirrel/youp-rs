815b4f866f9480be552b71d5961a691639ad0598
Kevin McCormack <kevin@mccormack.tech>
2024-09-27 14:20:19 -0400

    Add response time to output

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("URL: {:?}", args.url_string);
 
     loop {
-        let status = reqwest::get(&args.url_string)
-            .await?
-            .status();
+        let start: chrono::DateTime<chrono::Local> = chrono::offset::Local::now();
+        let res_future = reqwest::get(&args.url_string);
+        let response: reqwest::Response = reqwest::get(&args.url_string)
+            .await?;
+        let now: chrono::DateTime<chrono::Local> = chrono::offset::Local::now();
+        let status: reqwest::StatusCode = response.status();
+        let duration: chrono::TimeDelta = now - start;
+        let formatted_timestamp = format!("{}", now.format("%Y-%m-%d %H:%M:%S"));
 
-        let now = chrono::offset::Local::now();
-        let colored_status = util::colorize_status(status);
-        println!("{} Response status: {}", now, colored_status);
+        let colored_status: colored::ColoredString = util::colorize_status(status);
+        println!("{} Responded {} in {}ms", formatted_timestamp, colored_status, duration.num_milliseconds());
+
         thread::sleep(five_seconds);
     }
 
