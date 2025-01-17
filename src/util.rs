8290d302fcc8af7875fe3d6476690e73f58364a5
Kevin McCormack <kevin@mccormack.tech>
2025-01-16 23:49:17 -0500

    Add editorconfig

--- src/util.rs
+++ src/util.rs
@@ -2,12 +2,12 @@ use colored::{ColoredString, Colorize};
 use reqwest::StatusCode;
 
 pub fn colorize_status(status: StatusCode) -> ColoredString {
-  return match status.as_u16() {
-      200..=299 => status.to_string().green(),
-      400..=499 => status.to_string().yellow(),
-      500..=599 => status.to_string().red(),
-      _ => status.to_string().normal(),
-  };
+    return match status.as_u16() {
+        200..=299 => status.to_string().green(),
+        400..=499 => status.to_string().yellow(),
+        500..=599 => status.to_string().red(),
+        _ => status.to_string().normal(),
+    };
 }
 
 #[cfg(test)]
